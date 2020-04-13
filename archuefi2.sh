69b2103c0002e75d1adfa837ae032cfd674985d2
AlexeyKozma <46000958+AlexeyKozma@users.noreply.github.com>
2020-04-13 17:22:11 +0500

    Update

--- archuefi2.sh
+++ archuefi2.sh
@@ -68,7 +68,7 @@ cp /root/xorg.conf.new /etc/X11/xorg.conf
 echo "Какое DE ставим?"
 read -p "1 - XFCE, 2 - KDE, 3 - Openbox, Deepen - 4, 5 - GNOME " vm_setting
 if [[ $vm_setting == 1 ]]; then
-  pacman -S xfce4 xfce4-goodies xfce4-session xfce4-whiskermenu-plugin --noconfirm
+  pacman -S xfce4 xfce4-goodies xfce4-session xfce4-whiskermenu-plugin sddm --noconfirm
 elif [[ $vm_setting == 2 ]]; then
   pacman -Sy plasma --noconfirm
   pacman -Sy kde-applications-meta --noconfirm
@@ -88,6 +88,8 @@ if [[ $vm_setting == 1 ]]; then
   #pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
   #systemctl start lightdm
   #systemctl enable lightdm
+  systemctl start sddm
+  systemctl enable sddm
 fi
 
 if [[ $vm_setting == 2 ]]; then
