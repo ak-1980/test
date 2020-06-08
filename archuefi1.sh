86f8dee78cf4f7d5e1f92031474da9d4d20bdc3f
AlexeyKozma <46000958+AlexeyKozma@users.noreply.github.com>
2020-06-09 02:14:47 +0500

    update

--- archuefi1.sh
+++ archuefi1.sh
@@ -18,7 +18,7 @@ echo '2.4 создание разделов'
  echo n;
  echo;
  echo;
- echo +300M;
+ echo +520M;
  echo y;
  echo t;
  echo 1;
@@ -43,7 +43,7 @@ echo '2.4.2 Форматирование дисков'
 sd_1=$sd_disk'1'
 sd_2=$sd_disk'2'
 sd_3=$sd_disk'3'
-mkfs.fat -F32 /dev/$sd_1
+mkfs.vfat /dev/$sd_1
 mkfs.ext4  /dev/$sd_2
 mkfs.ext4  /dev/$sd_3
 
