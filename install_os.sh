a61934f293ab51ae6c9c876704268f0e1aa9bc0c
Alexey Kozma <alexeykozma@gmail.com>
2020-07-02 02:11:39 +0500

    Update -4

--- install_os.sh
+++ install_os.sh
@@ -90,6 +90,67 @@ function install_base()
     done
 }
 
+function configuration_os()
+{
+    clear
+    while [[ true ]]; do
+        arch-chroot /mnt
+        printf "###############\n
+        # 1) Установка имени компьтера\n# 2) Установка имя пользователя\n# 3) Настройка часового пояса\n# 4) Локализация\n#
+        5) Установка языка системы\n# 6) Установка шрифта для консоли\n# 7) Загрузочный RAM диск\n# 8) Установщик загрузчика\n# 
+        9) Установка программ для Wi-Fi\n# 10) Добавления пользователя\n# 11) Создание root пароля\n# 
+        # 12) Устанавка пароля пользователя\n# 13) Репозиторий multilib для работы 32-битных приложений\n# 14) Установка видеодрайверов\n"  
+        read -p ": " type_
+        case $type_ in 
+        1) read -p "Введите имя компьютера: " hostname  
+        echo $hostname > /etc/hostname ;;
+        2) read -p "Введите имя пользователя: " username ;;
+        3) echo "Установка часовго пояса по умолчанию.." 
+        ln -svf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime ;;
+        4) echo 'Добавляем русскую локаль системы'
+            echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+            echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
+            echo "ru_RU.KOI8-R KOI8-R" >> /etc/locale.gen
+            echo "ru_RU.CP1251 CP1251" >> /etc/locale.gen
+            echo "ru_RU ISO-8859-5" >> /etc/locale.gen 
+            echo 'Обновим текущую локаль системы'
+            locale-gen ;;
+        5)  echo 'Указываем язык системы'
+            echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf ;;  
+        6)  echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+            echo 'KEYMAP=ru' >> /etc/vconsole.conf
+            echo 'FONT=cyr-sun16' >> /etc/vconsole.conf ;;   
+        7)  echo 'Создадим загрузочный RAM диск'
+            mkinitcpio -p linux ;;  
+        8)  grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --force ;;     
+        9)  echo 'Ставим программу для Wi-fi'
+            pacman -S dialog wpa_supplicant --noconfirm ;;   
+        10) echo 'Добавляем пользователя'
+            useradd -m -g users -G wheel -s /bin/bash $username ;;  
+        11) echo 'Создаем root пароль'
+            passwd ;;  
+        12) echo 'Устанавливаем пароль пользователя'
+            passwd $username
+            echo 'Устанавливаем SUDO'
+            echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers ;;
+        13) echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+            echo '[multilib]' >> /etc/pacman.conf
+            echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+            pacman -Syy ;;
+        14) echo "Куда устанавливем Arch Linux на виртуальную машину?"
+            read -p "1 - Да, 0 - Нет: " vm_setting
+            if [[ $vm_setting == 0 ]]; then
+            pacman -S xorg-server xorg-drivers xorg-utils xorg-apps xorg-xinit
+            elif [[ $vm_setting == 1 ]]; then
+            pacman -S xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils
+            fi
+            Xorg :0 -configure
+            cp /root/xorg.conf.new /etc/X11/xorg.conf  ;;              
+        esac
+        exit  
+    done
+}
+
 # menu 1) Информация облочных устройствых (Дисках)
 
 
@@ -100,14 +161,15 @@ function menu_install()
     # Вывод инвормации 
     while [[ true ]]
     do
-    printf "############# \n# 1) разметка жесткого диска\n# 2) Выбор зекал\n# 3) Установка основных пакетов \n# 
-    4) Выход \n############# Ввод: "
+    clear
+    printf "############# \n# 1) разметка жесткого диска\n# 2) Выбор зекал\n# 3) Установка основных пакетов \n
+    # 4) Базовые настройки # 5) Выход \n############# Ввод:"
     read -p "  " exit_e
         case $exit_e in 
         1) diskor ;;
         2) mirrors ;;
         3) install_base ;;
-        4) ;;
+        4) configuration_os ;;
         5) break ;;
         esac
     done
